8ba1950fc0a54d2a07a0db2fdca6235d56d260ab
Ben Marx <benyamin.marx@gmail.com>
2016-07-16 23:39:42 -0400

    Initial setup

--- mix.exs
+++ mix.exs
@@ -18,8 +18,14 @@ defmodule Issues.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Issues, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+     applications: [:cowboy,
+                    :gettext,
+                    :logger,
+                    :phoenix,
+                    :phoenix_ecto,
+                    :phoenix_html,
+                    :phoenix_pubsub,
+                    :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +36,14 @@ defmodule Issues.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
+    [{:cowboy, "~> 1.0"},
+     {:gettext, "~> 0.11"},
+     {:phoenix, "~> 1.2.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:phoenix_pubsub, "~> 1.0"},
+     {:postgrex, ">= 0.0.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
